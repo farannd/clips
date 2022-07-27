bcffa8be59adf4e42107a2aa88630f21ac253234
Farand Farhansyah <farand.farhansyah@bankmandiri.co.id>
2022-07-27 14:47:03 +0700

    finish section 7

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AuthModalComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
